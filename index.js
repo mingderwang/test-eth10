a50bff684eeded0b342595f66fd8c2597938c50b
Ming-der Wang <47238+mingderwang@users.noreply.github.com>
2023-02-05 06:39:46 +0000

    use mithril

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const eth10 = require('@muzamint/eth10')
+const { Thing } = require("@muzamint/eth10");
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+  res.write(`<body>
+    <script src="https://unpkg.com/mithril/mithril.js"></script>
+    <script>
+    var root = document.body
+    m.mount(root, {
+    view: function() {
+        return m("h1", "Try me out")
+    }
+})
+    </script>
+</body>`);
+  res.end()
 });
 
 app.listen(3000, () => {
